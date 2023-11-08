777ca4efcfc9b54f8b5dcce4f872d0f64c0b17f5
NationofFoxes <stuart.william.auld@gmail.com>
2023-11-08 23:46:09 +0900

    phone breakpoints

--- _nuxt/home.68aa16f0.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as o,i as s,a,o as c}from"./entry.7cbd3060.js";const n={},_={class:"py-20 px-10 text-center"},r=a("div",{class:"mt-5 mx-auto text-center opacity-25 text-sm"}," [Home Layout] ",-1);function l(e,m){return c(),o("main",_,[s(e.$slots,"default"),r])}const i=t(n,[["render",l]]);export{i as default};
